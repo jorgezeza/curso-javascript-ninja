f6deba1ba3f56bdae7c0aa14b3a99edc27f0b64e
Jorge Nunes <jorgezeza2010@gmail.com>
2019-01-03 05:55:15 -0200

    challenge-21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -1,17 +1,54 @@
-/*
-O desafio de hoje será um pequeno projeto: um cronômetro!
-As regras para criação do cronômetro são as seguintes:
-1. Crie um arquivo index.html e adicione esse script a ele;
-2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
-Ele será o nosso cronômetro;
-3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
-4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
-cada segundo;
-5. Ao clicar em Stop, o cronômetro deve parar de contar;
-6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+(function (win, doc) {
+    'use strick';
+    /*
+    O desafio de hoje será um pequeno projeto: um cronômetro!
+    As regras para criação do cronômetro são as seguintes:
+    1. Crie um arquivo index.html e adicione esse script a ele;
+    2. Crie um campo `input` do tipo `text`, e inicie-o com um valor 0 (zero).
+    Ele será o nosso cronômetro;
+    3. Crie 3 botões para as ações do cronômetro: Start, Stop e Reset;
+    4. Ao clicar em Start, o valor do campo deve ser incrementado de 1 em 1, a
+    cada segundo;
+    5. Ao clicar em Stop, o cronômetro deve parar de contar;
+    6. Ao clicar em Reset, o cronômetro deve zerar e parar de contar.
+    
+    Utilize o atributo data-js para nomear o campo e os botões. Você pode
+    usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
+    dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
+    */
+    // ?
 
-Utilize o atributo data-js para nomear o campo e os botões. Você pode
-usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
-dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
-*/
-// ?
+    var $btnStart = document.querySelector('[data-js="start"]');
+    var $btnStop = document.querySelector('[data-js="stop"]');
+    var $btnReset = document.querySelector('[data-js="reset"]');
+    var $inputCronomentro = doc.querySelector('[data-js="input-cronometro"]');
+
+    var temp;
+    var count = 0;
+
+    $btnStart.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        function time() {
+            temp = setTimeout(time, 1000)
+            $inputCronomentro.value = count++;
+        }
+
+        time();
+    }, false);
+
+    $btnStop.addEventListener('click', function (event) {
+        event.preventDefault();
+        clearTimeout(temp);
+    }, false);
+
+
+    $btnReset.addEventListener('click', function (event) {
+        event.preventDefault();
+        $inputCronomentro.value = 0;
+        clearTimeout(temp);
+        temp = 0;
+        count = 0;
+    });
+
+})(window, document);
